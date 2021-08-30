88728d7482d16da44e55d5de5aa0ac64146fd868
dgsr <hermanislamov@gmail.com>
2021-08-30 18:37:33 +0300

    hw1 template for all tasks

---
+++ homework1/list_task/check_sums.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def check_sum_of_four(a: List[int], b: List[int],
+                      c: List[int], d: List[int]) -> int:
+    pass
---
+++ homework1/max_min/maxmin.py
@@ -0,0 +1,5 @@
+from typing import Tuple
+
+
+def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
+    pass
