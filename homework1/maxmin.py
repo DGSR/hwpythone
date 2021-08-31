e19e03281a32414e959847a41bb2b74262e319ad
dgsr <hermanislamov@gmail.com>
2021-08-31 23:04:10 +0300

    hw1 task3 maxmin

--- homework1/maxmin.py
+++ homework1/maxmin.py
@@ -2,4 +2,17 @@ from typing import Tuple
 
 
 def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
-    pass
+    """
+    Function returns max and min values in file
+    search and comparison is done line by line
+    start values are those in first line of the file
+    """
+    with open(file_name, 'r') as f:
+        maxN = int(f.readline().strip())
+        minN = maxN
+        for line in f:
+            if int(line.strip()) > maxN:
+                maxN = int(line.strip())
+            elif int(line.strip()) < minN:
+                minN = int(line.strip())
+    return (maxN, minN)
---
+++ tests/homework1/test_maxmin.py
@@ -0,0 +1,8 @@
+from homework1.maxmin import find_maximum_and_minimum as find_max_min
+import os.path as oss
+
+
+def test_find_max_min():
+    our_dir = oss.dirname(__file__)
+    assert find_max_min(oss.join(our_dir, "2.txt")) == (15, 1)
+    assert find_max_min(oss.join(our_dir, "3.txt")) == (10000, 1096)
