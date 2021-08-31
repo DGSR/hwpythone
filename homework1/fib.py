3f10def2829b3d7566397ebff363a67f91c49635
dgsr <hermanislamov@gmail.com>
2021-08-31 17:07:49 +0300

    restructure tests and hw1 task2 fib

--- homework1/fib.py
+++ homework1/fib.py
@@ -2,4 +2,17 @@ from typing import Sequence
 
 
 def check_fibonacci(data: Sequence[int]) -> bool:
-    pass
+    if len(data) == 0:
+        return False
+    temp = 0
+    temp1 = 1
+    while temp < data[0]:
+        temp, temp1 = temp1, temp + temp1
+    if len(data) > 1:
+        if data[0] == 1 and data[1] != 1:
+            temp, temp1 = temp1, temp + temp1
+    for i in data:
+        if temp != i:
+            return False
+        temp, temp1 = temp1, temp + temp1
+    return True
