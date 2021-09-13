eea62e0082c9373ac5f721e93107cd469a7fe2ab
dgsr <hermanislamov@gmail.com>
2021-09-13 19:51:44 +0300

    task 1

---
+++ tests/homework3/test_task1.py
@@ -0,0 +1,17 @@
+from mock import patch
+
+from homework3.task1 import cache
+
+
+@cache(times=2)
+def f():
+    return input('? ')
+
+
+def test_cache():
+    with patch('builtins.input', return_value="1"):
+        assert f() == '1'
+    assert f() == '1'
+    assert f() == '1'
+    with patch('builtins.input', return_value="2"):
+        assert f() == '2'
