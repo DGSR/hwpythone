6232c34d7657e63cf074f28452c1f80780c51d86
dgsr <hermanislamov@gmail.com>
2021-09-19 18:59:53 +0300

    hw4 task 1 and 5

---
+++ homework4/task1.py
@@ -0,0 +1,12 @@
+def read_magic_number(path: str) -> bool:
+    """
+    reads first line of file, if it is in [1, 3) returns True
+    otherwise False
+    any error is raised as ValueError
+    """
+    try:
+        with open(path, "r", encoding='utf-8') as file:
+            temp = file.readline()
+        return 1.0 <= float(temp) < 3.0
+    except (ValueError, FileNotFoundError):
+        raise ValueError("file not found or first line value is wrong")
---
+++ homework4/task5.py
@@ -0,0 +1,15 @@
+from typing import Generator
+
+
+def get_value_for_fizzbuzz(val: int) -> str:
+    """returns value for fizzbuzz generator using mod 15 and dictionary"""
+    dict = {1: val, 2: val, 3: "fizz", 4: val, 5: "buzz",
+            6: "fizz", 7: val, 8: val, 9: "fizz", 10: "buzz",
+            11: val, 12: "fizz", 13: val, 14: val, 0: "fizzbuzz"}
+    return str(dict[val % 15])
+
+
+def fizzbuzz(number: int) -> Generator[str, str, str]:
+    """returns generator of n fizzbuzz numbers, starting with 1 without if"""
+    for num in range(1, number+1):
+        yield get_value_for_fizzbuzz(num)
