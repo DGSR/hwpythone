6b23e0d1681d53f74f3c925a5e99d7b7871cdd73
dgsr <hermanislamov@gmail.com>
2021-09-19 13:13:36 +0300

    hw4 task 3 docstring

--- homework4/task3.py
+++ homework4/task3.py
@@ -1,5 +1,6 @@
 import sys
 
 
-def my_precious_logger(text: str):
+def my_precious_logger(text: str) -> None:
+    """prints to stderr if text starts with error otherwise to stdout"""
     print(text, file=sys.stderr if text.startswith("error") else sys.stdout)
