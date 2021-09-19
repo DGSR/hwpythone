fbe4bf27241d01c791c4cffea62346c76491ee70
dgsr <hermanislamov@gmail.com>
2021-09-19 23:20:03 +0300

    save original info

---
+++ homework5/save_original_info.py
@@ -0,0 +1,32 @@
+import functools
+
+
+def save_original_info(orig_func):
+    """
+    parameterized decorator, which saves info about initial function
+    """
+    def wrapper(func):
+        def substitute(*args, **kwargs):
+            return func(*args, **kwargs)
+        substitute.__original_func = orig_func
+        substitute.__name__ = orig_func.__name__
+        substitute.__doc__ = orig_func.__doc__
+        return substitute
+    return wrapper
+
+
+def print_result(func):
+    @save_original_info(func)
+    def wrapper(*args, **kwargs):
+        """Function-wrapper which print result of an original function"""
+        result = func(*args, **kwargs)
+        print(result)
+        return result
+
+    return wrapper
+
+
+@print_result
+def custom_sum(*args):
+    """This function can sum any objects which have __add___"""
+    return functools.reduce(lambda x, y: x + y, args)
---
+++ tests/homework5/test_save_original_info.py
@@ -0,0 +1,11 @@
+from homework5.save_original_info import custom_sum
+
+
+def test_save_original_info(capsys):
+    custom_sum(1, 2, 3, 4)
+    without_print = custom_sum.__original_func
+    captured = capsys.readouterr()
+    assert captured.out == str(without_print(1, 2, 3, 4))+"\n"
+    assert custom_sum.__doc__ == "This function can sum "\
+                                 "any objects which have __add___"
+    assert custom_sum.__name__ == "custom_sum"
