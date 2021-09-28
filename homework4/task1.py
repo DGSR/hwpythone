4059961b2a7dae01adae1cb04a96a1f4a92a4f7a
dgsr <hermanislamov@gmail.com>
2021-09-28 16:22:34 +0300

    hw4 corrections 1

--- homework4/task1.py
+++ homework4/task1.py
@@ -1,12 +1,17 @@
-def read_magic_number(path: str) -> bool:
+from constant.constants import DEFAULT_ENCODING
+
+
+def read_magic_number(path: str, lower_bound: int = 1.0,
+                      upper_bound: int = 3.0) -> bool:
     """
-    reads first line of file, if it is in [1, 3) returns True
-    otherwise False
+    reads first line of file, if it is in between lower and upper bounds
+     (upper bound excluded)
+     returns True otherwise False
     any error is raised as ValueError
     """
     try:
-        with open(path, 'r', encoding='utf-8') as file:
+        with open(path, 'r', encoding=DEFAULT_ENCODING) as file:
             temp = file.readline()
-        return 1.0 <= float(temp) < 3.0
+        return lower_bound <= float(temp) < upper_bound
     except (ValueError, FileNotFoundError):
         raise ValueError('file not found or first line value is wrong')
--- homework4/task2.py
+++ homework4/task2.py
@@ -1,15 +1,16 @@
 import requests
 from requests.exceptions import RequestException
 from requests.models import HTTPError
+from constant.constants import CHAR_CONSTANT, DEFAULT_ENCODING
 
 
-def count_dots_on_i(url: str, char_search: str = 'i') -> int:
+def count_dots_on_i(url: str, char_search: str = CHAR_CONSTANT) -> int:
     """
     counts given letter in html by given url (by default letter i)
     in case of any network raises ValueError
     """
     try:
-        res = requests.get(url).content.decode('utf-8')
+        res = requests.get(url).content.decode(DEFAULT_ENCODING)
     except (ConnectionError, TimeoutError, RequestException, HTTPError):
         raise ValueError('Unreachable {url})')
     return res.count(char_search)
