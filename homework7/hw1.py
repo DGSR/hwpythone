a00b90cb2afbe01fb6b273381dac22b61b104518
dgsr <hermanislamov@gmail.com>
2021-10-01 18:52:42 +0300

    hw7 corrections 1

--- homework7/hw1.py
+++ homework7/hw1.py
@@ -2,22 +2,16 @@ from collections.abc import Iterable
 from typing import Any
 
 
-def is_iterable(arg: Any) -> bool:
-    """
-    return True if argument is iterable but not string
-    """
-    return isinstance(arg, Iterable) and not isinstance(arg, str)
-
-
-def find_occurrences(tree: dict, element: Any) -> int:
+def find_occurrences(tree: dict, element: Any, count: int = 0) -> int:
     """
     returns number of occurences of element in dict and its nested structures
     """
-    count = 0
+    is_iterable = isinstance(tree, Iterable) and not isinstance(tree, str)
     if isinstance(tree, dict):
-        count += sum([find_occurrences(val, element) for val in tree.values()])
-    elif is_iterable(tree):
-        count += sum([find_occurrences(val, element) for val in tree])
+        count += sum([find_occurrences(val, element, count)
+                      for val in tree.values()])
+    elif is_iterable:
+        count += sum([find_occurrences(val, element, count) for val in tree])
     else:
         count += 1 if tree == element else 0
     return count
--- homework7/hw2.py
+++ homework7/hw2.py
@@ -1,18 +1,9 @@
-def sign_replacement(string: str, sign: str = '#') -> str:
-    """
-    return string where each given sign and a symbol before is deleted
-    """
-    res = ''
-    for i in string:
-        if i == sign:
-            res = res[:-1]
-        else:
-            res += i
-    return res
+from re import sub
 
 
 def backspace_compare(first: str, second: str) -> bool:
     """
     return if given strings are equal after deleting # sign and a symbol before
     """
-    return sign_replacement(first) == sign_replacement(second)
+    pattern = r'((\w)?#)+'
+    return sub(pattern, '', first) == sub(pattern, '', second)
