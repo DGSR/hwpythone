ec0b4bb0f87110caae62baaf36ecdcc522e7db2a
dgsr <hermanislamov@gmail.com>
2021-10-11 19:51:58 +0300

    hw7 task3 correction

--- homework7/hw3.py
+++ homework7/hw3.py
@@ -1,19 +1,13 @@
 from typing import List
 
+from constant.tic_tac_toe_victory_conditions import VICTORY_CONDITIONS
+
 
 def tic_tac_toe_checker(board: List[List]) -> str:
     """
     return result of checking tic-tac-toe board
     """
-    victory_conditions = [[(0, 0), (0, 1), (0, 2)],
-                          [(1, 0), (1, 1), (1, 2)],
-                          [(2, 0), (2, 1), (2, 2)],
-                          [(0, 0), (1, 0), (2, 0)],
-                          [(0, 1), (1, 1), (2, 1)],
-                          [(0, 2), (1, 2), (2, 2)],
-                          [(0, 0), (1, 1), (2, 2)],
-                          [(0, 2), (1, 1), (2, 0)]]
-    for row in victory_conditions:
+    for row in VICTORY_CONDITIONS:
         board_elems = [board[elem[0]][elem[1]] for elem in row]
         if board_elems.count(board_elems[0]) == 3:
             return f'{board_elems[0]} wins!'
