5565c641e38e1672f5102beeb0dff6a0460c8450
dgsr <hermanislamov@gmail.com>
2021-10-23 01:10:21 +0300

    hw10 async and tests

--- homework10/stonks.py
+++ homework10/stonks.py
@@ -1,7 +1,8 @@
 import json
+from concurrent.futures import ThreadPoolExecutor
 from operator import attrgetter
 from re import search
-from typing import Any, List
+from typing import Any, List, Tuple
 from xml.etree import ElementTree
 
 import requests
@@ -47,12 +48,27 @@ class Stonks:
         self.lowest_p_e = []
         self.highest_growth = []
         self.highest_high_low = []
+        self.main_site = 'https://markets.businessinsider.com'
+        self.stats_to_find = (
+            ('latest_price', True),
+            ('p_e', False),
+            ('year_growth', True),
+            ('high_low_diff', True)
+        )
+        self.filenames_stats = (
+            'expensive_price.json',
+            'lowest_p_e.json',
+            'highest_growth.json',
+            'highest_high_low.json'
+        )
 
     def request_contents(self, url: str) -> bytes:
         """
         return contents of page for given url
         """
-        return requests.get(url).content
+        response = requests.get(url)
+        data = response.content
+        return data
 
     def strip_digit(self, string: str) -> float:
         """
@@ -72,82 +88,89 @@ class Stonks:
         root = ElementTree.fromstring(res)
         self.exchange_rate = float(root.find(XML_QUERY).text.replace(',', '.'))
 
+    def parse_initial_info(self, row) -> None:
+        """
+        parse html from website to get and store info about company
+        """
+        url = self.main_site + row[0][0].get('href')
+        latest_price = self.strip_digit(row[1].text) * self.exchange_rate
+        year_growth = (self.strip_digit(row[-1][2].text[:-1])
+                       * self.exchange_rate)
+        cmp = Company(url=url, latest_price=latest_price,
+                      year_growth=year_growth)
+        self.companies.append(cmp)
+
     def get_companies_from_table(self) -> None:
         """
-        parse html from website to get info about companies
-         and store them
+        get companies and initial data in parallel
         """
-        contents = html.fromstring(self.request_contents(self.snp500_link))
+        response = self.request_contents(self.snp500_link)
+        contents = html.fromstring(response)
         page_rows = contents.xpath('//tr')[1:]
-        main_site = 'https://markets.businessinsider.com'
-        for row in page_rows:
-            url = main_site + row[0][0].get('href')
-            latest_price = self.strip_digit(row[1].text) * self.exchange_rate
-            year_growth = (self.strip_digit(row[-1][2].text[:-1])
+        with ThreadPoolExecutor(max_workers=len(page_rows)) as pool:
+            pool.map(self.parse_initial_info, page_rows)
+
+    def parse_company_details(self, company) -> None:
+        """
+        parse html from company to get and store details about it
+        """
+        response = self.request_contents(company.url)
+        contents = html.fromstring(response)
+
+        name_label = contents.xpath(NAME_LABEL_QUERY)[0].text.strip()
+        code_label = contents.xpath(CODE_LABEL_QUERY)[0].text[2:]
+
+        low_label = contents.xpath(HIGH_LOW_LABEL_QUERY)[0][0].text
+        low_number = self.strip_digit(low_label)
+
+        high_label = contents.xpath(HIGH_LOW_LABEL_QUERY)[0][1].text
+        high_number = self.strip_digit(high_label)
+
+        p_e_label = contents.xpath(P_E_LABEL_QUERY)
+
+        company.name = name_label
+        company.company_code = code_label
+        company.high_low_diff = (low_number / high_number * 100
+                                 * self.exchange_rate)
+        if p_e_label:
+            company.p_e = (self.strip_digit(p_e_label[0].text)
                            * self.exchange_rate)
-            cmp = Company(url=url, latest_price=latest_price,
-                          year_growth=year_growth)
-            self.companies.append(cmp)
 
     def get_company_details(self) -> None:
         """
-        parse html from company to get details about companies
-         and store them
+        get details about companies in parallel
         """
-        for company in self.companies:
-            contents = html.fromstring(self.request_contents(company.url))
+        with ThreadPoolExecutor(max_workers=len(self.companies)) as pool:
+            pool.map(self.parse_company_details, self.companies)
 
-            name_label = contents.xpath(NAME_LABEL_QUERY)[0].text.strip()
-            code_label = contents.xpath(CODE_LABEL_QUERY)[0].text[2:]
-
-            low_label = contents.xpath(HIGH_LOW_LABEL_QUERY)[0][0].text
-            low_number = self.strip_digit(low_label)
-
-            high_label = contents.xpath(HIGH_LOW_LABEL_QUERY)[0][1].text
-            high_number = self.strip_digit(high_label)
-
-            p_e_label = contents.xpath(P_E_LABEL_QUERY)
-
-            company.name = name_label
-            company.company_code = code_label
-            company.high_low_diff = (low_number / high_number * 100
-                                     * self.exchange_rate)
-            if p_e_label:
-                company.p_e = (self.strip_digit(p_e_label[0].text)
-                               * self.exchange_rate)
-
-    def top_10_by_attr(self, attr: str, reverse: bool = True) -> List[Company]:
+    def top_10_by_attr(self, args: Tuple[str, bool]) -> List[Company]:
         """
         return sorted list of classes by given attribute
          order descending(default) and ascending
         """
-        return sorted(self.companies, key=attrgetter(attr),
+        attribute, reverse = args
+        return sorted(self.companies, key=attrgetter(attribute),
                       reverse=reverse)[:10]
 
-    def stats_top_10(self) -> None:
-        """
-        get top 10 stats for companies by different parameters
-        """
-        self.expensive_price = self.top_10_by_attr('latest_price')
-        self.lowest_p_e = self.top_10_by_attr('p_e', False)
-        self.highest_growth = self.top_10_by_attr('year_growth')
-        self.highest_high_low = self.top_10_by_attr('high_low_diff')
-
-    def save_list_to_json(self, filename: str, arr: List[Any]) -> None:
+    def save_list_to_json(self, args: Tuple[str, List[Any]]) -> None:
         """
         write to file list of classes, json style
         """
+        filename, arr = args
         with open(filename, 'w') as file:
             json.dump([cmp.__dict__ for cmp in arr], file, indent=4)
 
     def save_stats_to_json(self) -> None:
         """
-        write top 10 stats to files
+        write top 10 stats to files using threads
         """
-        self.save_list_to_json('expensive_price.json', self.expensive_price)
-        self.save_list_to_json('lowest_p_e.json', self.lowest_p_e)
-        self.save_list_to_json('highest_growth.json', self.highest_growth)
-        self.save_list_to_json('highest_high_low.json', self.highest_high_low)
+        with ThreadPoolExecutor(max_workers=len(self.stats_to_find)) as pool:
+            stats = pool.map(self.top_10_by_attr, self.stats_to_find)
+
+        file_and_stats = list(zip(self.filenames_stats, stats))
+
+        with ThreadPoolExecutor(max_workers=len(file_and_stats)) as pool:
+            pool.map(self.save_list_to_json, file_and_stats)
 
     def proccess(self) -> None:
         """
@@ -156,5 +179,4 @@ class Stonks:
         self.get_exchange_rate()
         self.get_companies_from_table()
         self.get_company_details()
-        self.stats_top_10()
         self.save_stats_to_json()
---
+++ tests/homework10/test_stonks.py
@@ -0,0 +1,53 @@
+from unittest.mock import MagicMock, Mock, patch
+
+from homework10.stonks import Stonks
+
+
+def test_stonks_strip_digit():
+    assert Stonks.strip_digit(Stonks, '\\r\\n2,048') == 2048.0
+    assert Stonks.strip_digit(Stonks, 'Hello0.451Darkness') == 0.451
+
+
+def test_stonks_exchange_rate():
+    tmp = Stonks()
+    xml = '<Something><Valute ID="R01235">'\
+          '<Value>30,05</Value>'\
+          '</Valute></Something>'
+    with patch('homework10.stonks.Stonks.request_contents', return_value=xml):
+        tmp.get_exchange_rate()
+        assert tmp.exchange_rate == 30.05
+
+
+def test_parse_initial_info():
+    tmp = Stonks()
+    tmp.exchange_rate = 1.0
+    tmp.main_site = 'main'
+
+    mock = Mock()
+    mock.text = '10'
+
+    row = [[{'href': '!'}], mock, [0, 0, mock]]
+    tmp.parse_initial_info(row)
+    assert tmp.companies[0].url == 'main!'
+    assert tmp.companies[0].latest_price == 10.0
+    assert tmp.companies[0].year_growth == 1.0
+
+
+def test_parse_company_details():
+    tmp = Stonks()
+    tmp.exchange_rate = 1.0
+    mock1 = MagicMock()
+    mock1[0].text = '335'
+    mock1[0][0].text = '10'
+
+    mock = MagicMock()
+    mock.xpath = lambda _: mock1
+
+    with patch('homework10.stonks.Stonks.request_contents', return_value=''), \
+         patch('lxml.html.fromstring', return_value=mock):
+        cmp = Mock()
+        tmp.parse_company_details(cmp)
+        assert cmp.name == '335'
+        assert cmp.company_code == '5'
+        assert cmp.high_low_diff == 100.0
+        assert cmp.p_e == 335.0
